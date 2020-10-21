24dca2dd77699977106dc66ad8c4988f31fd771c
ducviethaboo <70750327+ducviethaboo@users.noreply.github.com>
2020-10-21 16:02:14 +0700

    Add bai tap anh Luan giao

---
+++ tinhtientaxi.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<form method="post">
+    <input type="text" name="kilometter" placeholder="Kilometter">
+    <input type='submit' value="Pay">
+</form>
+    <?php
+    class differenceNumber extends Exception {}
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $km = $_REQUEST['kilometter'];
+        function checkKM($km)
+        {
+            if (is_numeric($km)) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        function calculateMoney($km)
+        {
+            if ($km > 0 && $km < 2) {
+                echo '<br>' . "Total : " . $km * 15000 . 'VND';
+            } elseif ($km > 1 && $km < 6) {
+                echo '<br>' . "Total : " . $km * 13500 . 'VND';
+            } elseif ($km >= 6 && $km < 120) {
+                echo '<br>' . "Total : " . $km * 11000 . 'VND';
+            } elseif ($km >= 120) {
+                echo '<br>' . "Total :" . $km * 11000 * 0.9 . 'VND';
+            } elseif ($km < 0 || checkKM($km) == false) {
+                echo '<br>' . "Please enter a valid number";
+            }
+        }
+
+        calculateMoney($km);
+    }
+    ?>
+</body>
+</html>
